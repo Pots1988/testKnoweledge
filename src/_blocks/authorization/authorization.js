902ed3b966403d51d7a47af8e6ba1a94254fc588
0989911197@ukr.net <0989911197@ukr.net>
2018-01-17 08:59:03 +0200

    first screen

--- src/_blocks/authorization/authorization.js
+++ src/_blocks/authorization/authorization.js
@@ -1,1 +1,17 @@
-document.addEventListener("DOMContentLoaded", submitAuthorization);
+document.addEventListener('DOMContentLoaded', function(){
+  var screen = document.querySelector("[data-authorization]");
+  if (screen) {
+    var form = screen.querySelector("[data-authorization-form]"),
+        userName = form.querySelector("[data-authorization-name]"),
+        userSurname = form.querySelector("[data-authorization-surname]"),
+        userEmail = form.querySelector("[data-authorization-email]");
+
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+      localStorage.setItem("userName", userName.value);
+      localStorage.setItem("userSurname", userSurname.value);
+      localStorage.setItem("userEmail", userEmail.value);
+      screen.classList.add("authorization--hidden");
+    })
+  }
+});
