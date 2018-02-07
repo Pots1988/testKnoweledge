3610814f31199c41bf08d12be617ead59ed6ee12
pots1988bear@gmail.com <pots1988bear@gmail.com>
2018-02-07 10:42:58 +0200

    Add localStorage get function

--- src/_blocks/authorization/authorization.js
+++ src/_blocks/authorization/authorization.js
@@ -1,17 +1,36 @@
 let authorize = document.querySelector("[data-authorization]");
 
-if(authorize) {
+function web_storage() {
+  try {
+    return "localStorage" in window && window["localStorage"] !== null;
+} catch (e) {
+    return false;
+  }
+}
+
+if (authorize) {
   let form = authorize.querySelector("[data-authorization-form]"),
       userName = form.querySelector("[data-authorization-name]"),
       userSurname = form.querySelector("[data-authorization-surname]"),
       userEmail = form.querySelector("[data-authorization-email]");
 
+  if (web_storage()) {
+    if (localStorage.getItem("userName")) {
+      userName.value = localStorage.getItem("userName");
+      userSurname.value = localStorage.getItem("userSurname");
+      userEmail.value = localStorage.getItem("userEmail");
+      form.querySelector("[type='submit']").focus();
+    }
+  }
+
   form.addEventListener("submit", e => {
     e.preventDefault();
 
-    localStorage.setItem("userName", userName.value);
-    localStorage.setItem("userSurname", userSurname.value);
-    localStorage.setItem("userEmail", userEmail.value);
+    if (web_storage()) {
+      localStorage.setItem("userName", userName.value);
+      localStorage.setItem("userSurname", userSurname.value);
+      localStorage.setItem("userEmail", userEmail.value);
+    }
 
     authorize.classList.add("authorization--hidden");
   });
